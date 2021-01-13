41b242ebcf9fb370d6cfef531dce90370cc025de
Bryan Romero <bryanromerou@gmail.com>
2021-01-12 21:14:22 -0800

    add about component

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const AboutCard = () => {
+const AboutCard = ({number, title, text}) => {
   return (
-    <div>
-      About Cards
+    <div className="about-card">
+      <p className="about-number">{number}</p>
+      <p className="card-title">{title}</p>
+      <p className="card-text">{text}</p>
     </div>
   );
 }
